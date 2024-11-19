de931138992761329ef09834bdb1bc7b58e96342
Khang <34341990+toni8699@users.noreply.github.com>
2024-11-19 17:00:52 -0500

    more exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let sum=0;
+    if (!Number.isInteger(a) || !Number.isInteger(b)||a<0||b<0){
+        return "ERROR";}
+    for (let i=Math.min(a,b);i<=Math.max(a,b);i++){
+        sum+=i;
+    }
+    return sum;
+    
 
 };
 
 // Do not edit below this line
+console.log(Number.isInteger(3));
 module.exports = sumAll;
